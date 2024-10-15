969565c9ff0a00ed2dd0dc25e1d5f6a243f5b3be
Sett Sarverott <sarverott@gmail.com>
2024-10-16 01:41:21 +0200

    upgrading builder.sh and deployer.sh, fixing missing version data in cmake, now it's passed by npm

--- dev/builder.sh
+++ dev/builder.sh
@@ -1,2 +1,12 @@
-cmake . 
+
+
+# for beginning developers of rattish implementations
+
+git add *
+
+read commit_note
+
+git commit -m "$commit_note"
+
+cmake . -D RATTISH_VERSION=${npm version patch}
 make 
---
+++ dev/deployer.sh
@@ -0,0 +1,14 @@
+
+# for beginning developers of rattish implementations
+
+nano README.md
+
+git add *
+
+read 'commit note: ' commit_note
+
+git commit -m "$commit_note"
+
+npm version minor
+
+git push
